e69c8c1b931c03d0051188446677d918e74259f5
thallysbraz <thallysbraz3@gmail.com>
2019-09-02 13:41:43 -0300

    arrumando identação do codigo

--- backend/src/config/routes.js
+++ backend/src/config/routes.js
@@ -1,12 +1,12 @@
-const express = require('express')
+const express = require("express");
 
 module.exports = function(server) {
 
     // API Routes
-    const router = express.Router()
-    server.use('/api', router)
+    const router = express.Router();
+    server.use("/api", router);
 
     // TODO Routes
-    const todoService = require('../api/todo/todoService')
-    todoService.register(router, '/todos')
+    const todoService = require("../api/todo/todoService");
+    todoService.register(router, "/todos");
 }
