9e71d99da374ddcddec73aa6ff20acd52bfb9e80
thallysbraz <thallysbraz3@gmail.com>
2019-09-23 13:33:33 -0300

    criando componente about e todo

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -2,4 +2,4 @@ const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
 
-module.exports = mongoose.connect("mongodb://localhost/todo");
+module.exports = mongoose.connect("mongodb://localhost/todo", { useNewUrlParser: true });
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     filename: "./app.js"
   },
   devServer: {
-    port: 8080,
+    port: 8088,
     contentBase: "./public"
   },
   resolve: {
