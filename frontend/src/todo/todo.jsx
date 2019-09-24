4a2b815bb1799e3489e90607c590f0cb5f8ccbee
thallysbraz <thallysbraz3@gmail.com>
2019-09-24 09:46:34 -0300

    listar e excluir description

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -14,14 +14,33 @@ export default class Todo extends Component {
 
     this.handleChange = this.handleChange.bind(this);
     this.handleAdd = this.handleAdd.bind(this);
+
+    this.handleRemove = this.handleRemove.bind(this);
+
+    this.refresh();
   }
+
+  refresh() {
+    axios
+      .get(`${URL}?sort=-createdAt`)
+      .then(resp =>
+        this.setState({ ...this.state, description: "", list: resp.data })
+      );
+  }
+
   handleChange(e) {
     this.setState({ ...this.state, description: e.target.value });
   }
   handleAdd() {
     const description = this.state.description;
-    axios.post(URL, { description }).then(resp => console.log("FUncionou!"));
+    axios.post(URL, { description }).then(resp => this.refresh());
   }
+  handleRemove(todo) {
+    axios
+      .delete(`${URL}/${todo._id}`)
+      .then(resp => this.refresh(this.state.description));
+  }
+
   render() {
     return (
       <div>
@@ -31,7 +50,7 @@ export default class Todo extends Component {
           handleChange={this.handleChange}
           handleAdd={this.handleAdd}
         />
-        <TodoList />
+        <TodoList list={this.state.list} handleRemove={this.handleRemove} />
       </div>
     );
   }
